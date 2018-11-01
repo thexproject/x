c8fe18f89498e9f7f03692e8df3042f319840793
Felix Mattick <felix.mattick@gmail.com>
2018-11-01 16:37:53 +0100

    Add everything

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const { src, dest } = require("gulp");
+const uglify = require("gulp-uglify-es").default;
+const rename = require("gulp-rename");
+const es = require("event-stream");
+
+exports.default = function() {
+  return es.merge(
+    src("src/*.js")
+      .pipe(uglify())
+      .pipe(rename({ extname: ".min.js" })),
+    src("src/*.js")
+  ).pipe(dest("dist/"));
+}
---
+++ src/x.js
@@ -0,0 +1,198 @@
+(() => {
+  // x - Lightweight DOM manipulation library
+
+  class xObject {
+    constructor(query) {
+      if (query instanceof Node) {
+        this.node = query;
+      } else if (query instanceof xObject) {
+        this.node = query.node;
+      } else {
+        if (query.startsWith("<")) {
+          query = query.substring(1);
+          if (query.endsWith(">")) {
+            query = query.substring(0, query.length - 1);
+          }
+          query = query.trim();
+          if (query.endsWith("/")) {
+            query = query.substring(0, query.length - 1);
+          }
+          query = query.trim();
+          this.node = document.createElement(query);
+        } else {
+          this.node = document.querySelector(query);
+        }
+      }
+    }
+
+    value(newValue) {
+      if (newValue === undefined) {
+        return this.node.value;
+      } else {
+        this.node.value = newValue;
+        return this;
+      }
+    }
+    text(newText) {
+      if (newText === undefined) {
+        return this.node.textContent;
+      } else {
+        this.node.textContent = newText;
+        return this;
+      }
+    }
+    html(newHtml) {
+      if (newHtml === undefined) {
+        return this.node.innerHTML;
+      } else {
+        this.node.innerHTML = newHtml;
+        return this;
+      }
+    }
+
+    append(html) {
+      let template = document.createElement("template");
+      template.innerHTML = html.trim();
+      let newNode = template.content.firstChild;
+
+      this.node.appendChild(newNode);
+      return this;
+    }
+    prepend(html) {
+      let template = document.createElement("template");
+      template.innerHTML = html.trim();
+      let newNode = template.content.firstChild;
+
+      this.node.insertBefore(newNode, this.node.firstChild);
+      return this;
+    }
+
+    appendTo(query) {
+      new xObject(query).append(this.node);
+      return this;
+    }
+    prependTo(query) {
+      new xObject(query).prepend(this.node);
+      return this;
+    }
+
+    classes(newClasses) {
+      if (newClasses === undefined) {
+        return this.node.className.split(" ");
+      } else {
+        this.node.className = newClasses.join(" ");
+        return this;
+      }
+    }
+    addClass(newClass) {
+      this.node.className += " ";
+      this.node.className += newClass;
+      return this;
+    }
+    id(newId) {
+      if (newId === undefined) {
+        return this.node.id;
+      } else {
+        this.node.id = newId;
+        return this;
+      }
+    }
+
+    style(styles) {
+      this
+        .append("<style>")
+        .append(styles.trim().replace(/\n/g, " "))
+        .append("</style>");
+      return this;
+    }
+    style(code) {
+      this
+        .append("<script>")
+        .append(code.trim())
+        .append("</script>");
+      return this;
+    }
+
+    destroy() {
+      this.node.parentNode.removeChild(this.node);
+    }
+
+    data(name) {
+      return this.node.dataset[name];
+    }
+
+    click(handler, that) {
+      if (handler === undefined) {
+        this.node.click();
+      } else {
+        this.node.onclick = () => {
+          handler(that);
+        }
+      }
+      return this;
+    }
+
+    find(query) {
+      return new xObject(this.node.querySelector(query));
+    }
+  }
+
+  window.x = query => {
+    return new xObject(query);
+  };
+
+  // xAct - Simple JavaScript templating library
+  
+  window.xAct = element => {
+    let html = x(element).html();
+    html = x("<textarea>").html(html).value();
+  
+    let templateRegex = /<<([^>>]+)?>>/g;
+    let blockRegex = /(^( )?(if|for|else|switch|case|break|var|let|const|{|}))(.*)?/g;
+      
+    let code = "var list = [];\n";
+    let index = 0;
+    let match;
+    
+    const append = (line, isJS) => {
+      line = line.trim();
+      if (isJS) {
+        code += line.match(blockRegex) ? line + "\n" : "list.push(" + line + ");\n";
+      } else {
+        code += line === "" ? "" : "list.push(\"" + line.replace(/"/g, "\\\"") + "\");\n";
+      }
+      return append;
+    }
+    
+    while (match = templateRegex.exec(html)) {
+      append(html.slice(index, match.index))(match[1], true);
+      index = match.index + match[0].length;
+    }
+    append(html.substr(index, html.length - index));
+    code += "return list.join(\"\");";
+
+    console.log("code", code);
+    
+    x(element).html(new Function(code.replace(/[\r\t\n]/g, "")).apply());
+  }
+
+  // xJax - A wrapper around fetch to make it simpler
+
+  window.xJax = async (uri, queries) => {
+    let queryString = "";
+    if (queries !== undefined) {
+      queryString += "?"
+      for (let key in queries) {
+        queryString += encodeURIComponent(key);
+        queryString += "=";
+        queryString += encodeURIComponent(queries[key]);
+        queryString += "&";
+      }
+      queryString = queryString.substring(0, str.length - 1);
+    }
+
+    let response = await fetch(uri + queryString);
+    let text = await response.text();
+    return text;
+  }
+})();
