b02aa6af4ea4600486bb88f9a9ce3af8cb1e60c3
Felix Mattick <felix.mattick@gmail.com>
2018-11-04 00:34:42 +0100

    Add console log

--- src/x.js
+++ src/x.js
@@ -158,6 +158,7 @@
         let previous = -1;
         for (let character of js) {
           if (previous < 0) previous = 0;
+          console.log(js[previous], character);
           if (quoteRegex.test(character) && js[previous] != "\\") isInString = !isInString;
           if (character == "=" && !isInString && hasBeginning) return true;
           previous++;
