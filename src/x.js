fab705ecb5d827a7718929a0d75a0e00bdebd879
Felix Mattick <felix.mattick@gmail.com>
2018-11-04 22:41:46 +0100

    Revert "Remove mouse leave event"
    This reverts commit 1eea898e3798bc1606350b1c6ab6c3ec47303c7b.

--- src/x.js
+++ src/x.js
@@ -147,6 +147,14 @@
       }
       return this;
     }
+    leave(handler, that) {
+      if (handler === undefined) {
+        this.node.click();
+      } else {
+        this.handle("blur", "mouseleave", handler, that);
+      }
+      return this;
+    }
 
     find(query) {
       return new xObject(this.node.querySelector(query));
