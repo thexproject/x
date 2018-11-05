f1ea235863310f8d9871f2e2512421ef81cc4fae
Felix Mattick <felix.mattick@gmail.com>
2018-11-05 16:19:57 +0100

    Make destroy() and on() chainable

--- src/x.js
+++ src/x.js
@@ -129,10 +129,12 @@
 
     on(eventName, handler, that) {
       this.node.addEventListener(eventName, event => { event.preventDefault(); handler(event, that); }, { passive: true, capture: true });
+      return this;
     }
     handle(touchEvent, mouseEvent, handler, that) {
       this.node.addEventListener(touchEvent, event => { event.preventDefault(); handler(that); }, { passive: true, capture: true });
       this.node.addEventListener(mouseEvent, event => { event.preventDefault(); handler(that); }, { passive: true, capture: true });
+      return this;
     }
     click(handler, that) {
       if (handler === undefined) {
